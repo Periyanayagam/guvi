1cf5f9638f567d327af156f5df683656dad01e03
Periya Nayagam <periyamydell@gmail.com>
2016-10-18 10:15:38 -0700

    First Commit

---
+++ sumOf.java
@@ -0,0 +1,16 @@
+class sumOf{
+	 
+	  public static void main(String[] args){
+		
+		  int sum=0;
+		  
+		  for(int i=0;i<5;i++){
+			sum += i;
+		  }
+		  
+		  System.out.println("Sum of 5 Natural number is "+sum);
+		  
+	  }
+} 
+	
+
