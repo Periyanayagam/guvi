8a93e80b9bdec18a795a8bbe541db12f0bfa706f
Periya Nayagam <periyamydell@gmail.com>
2016-10-18 09:16:38 -0700

    First Commit

---
+++ oddOrEven .java
@@ -0,0 +1,24 @@
+package crap;
+
+import java.util.Scanner;
+
+class oddOrEven {
+	 
+		 public static void main(String [] args) { 
+			 
+			 System.out.println("Enter a number..");
+			 Scanner s = new Scanner(System.in);
+			 int a = s.nextInt();
+			 
+			 if(a%2==0){
+				 System.out.printf("%d is an even number",a);
+			 }else{
+				 System.out.printf("%d is an odd number",a);
+			 }
+			 
+			 
+		 }
+
+} 
+	
+
